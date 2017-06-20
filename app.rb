5c83815a32fdabe28c51e8f55f33e42ab3c49469
Franklin Li <franklinli7609@gmail.com>
2017-06-20 19:57:16 +0000

    Revised day-after schedule

--- app.rb
+++ app.rb
@@ -10,6 +10,14 @@ class App < Sinatra::Base
     redirect to('/')
   end
 
+  get '/rainbow' do
+    erb :rainbow
+  end
+
+  get '/youtube' do
+    erb :youtube
+  end
+
   post '/subscribe' do
     @full_name = params[:full_name]
     @email = params[:email]
@@ -51,7 +59,15 @@ class App < Sinatra::Base
       ['6:30pm', 'Meetup Presentation'],
     ]
 
-    # TODO: add a third day's schedule (@day_after)
+    @day_after = [
+      ['6:15am', 'Wake up'],
+      ['6:30am', 'Work Out'],
+      ['8:00am', 'Breakfast'],
+      ['9:00am', 'Hardcore Coding'],
+      ['1:00pm', 'Lunch'],
+      ['3:00pm', 'Coffee Time'],
+      ['6:30pm', 'Bar'],
+    ]
 
     erb :schedule
   end
